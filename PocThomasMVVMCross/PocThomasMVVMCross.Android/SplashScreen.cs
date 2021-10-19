6cdb8e3663ea6a9b11c39a2e11423e5efc3b9f35
Thomas BERNARD <tbernard@pageup.fr>
2021-10-19 16:11:19 +0200

    Création d'un premier PoC sur android

---
+++ PocThomasMVVMCross/PocThomasMVVMCross.Android/SplashScreen.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Platforms.Android.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PocThomasMVVMCross.Android
+{
+    [Activity(Label = "PocThomasMVVMCross", MainLauncher = true)]
+    public class SplashScreen : MvxSplashScreenActivity
+    {
+        public SplashScreen() : base(Resource.Layout.SplashScreen)
+        {
+
+        }
+    }
+}
---
+++ PocThomasMVVMCross/PocThomasMVVMCross.Android/Views/FirstView.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Platforms.Android.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PocThomasMVVMCross.Android.Views
+{
+    [Activity(Label = "View for FirstViewModel")]
+    public class FirstView : MvxActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.FirstView);
+        }
+    }
+}
